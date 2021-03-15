2cb79451fbac1840d138a76d3d80785ecfec399b
harryghgim <harryghgim@gmail.com>
2021-03-15 23:32:32 +0900

    improved code added

--- 3.1.py
+++ 3.1.py
@@ -1,2 +1,19 @@
 hrs = input("Enter Hours:")
 h = float(hrs)
+rate = float(input("Enter Rate:"))
+if h > 40:
+    pay = 40 * rate + (h - 40) * rate * 1.5
+print(pay)
+
+# improved
+# hrs = float(input("Enter Hours: "))
+# rate = float(input("Enter Rate: "))
+# if hrs > 40:
+#     print("overtime")    
+#     reg = hrs * rate
+#     otp = (hrs - 40.0) * (rate * 0.5)
+#     pay = reg + otp
+# else:
+#     print("regular")
+#     pay = hrs * rate
+# print(pay)
