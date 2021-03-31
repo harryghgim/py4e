5698bcc8ff5018088cb1e06dcea28d1462da324a
harryghgim <harryghgim@gmail.com>
2021-03-31 17:40:56 +0900

    at draw method

--- projects/probability/prob_calculator.py
+++ projects/probability/prob_calculator.py
@@ -4,5 +4,31 @@ import random
 
 class Hat:
 
+    def __init__(self, **kwargs):
+        contents = list()
+        counts = dict()
+        for k in kwargs:
+            counts[k] = counts.get(k, 0) + 1
+        for k, v in counts.items():
+            for _ in range(v):
+                contents.append(k)
+                
+        self.contents = contents
+
+    def draw(self, nb):
+        population = self.contents
+        rmvlst = list()
+        lng = len(population)
+
+        if nb > lng: return population
+                
+        rmvlst = random.choices(population, k=nb)
+        for it in rmvlst:
+            self.contents.remove(it) # remove from original array
+
+        return rmvlst
+        
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    probability = 1
+    return probability
