b3fe7ab1e66b7bdeece8d713597bb9ed2cfe8e09
harryghgim <harryghgim@gmail.com>
2021-04-01 15:33:20 +0900

    probability done

--- projects/probability/prob_calculator.py
+++ projects/probability/prob_calculator.py
@@ -5,30 +5,56 @@ import random
 class Hat:
 
     def __init__(self, **kwargs):
-        contents = list()
-        counts = dict()
-        for k in kwargs:
-            counts[k] = counts.get(k, 0) + 1
-        for k, v in counts.items():
-            for _ in range(v):
-                contents.append(k)
-                
-        self.contents = contents
+        counts = kwargs.copy()
+        self.contents = [ k for k, v in counts.items() for _ in range(v) ]
+        self.dtcontents = counts
 
     def draw(self, nb):
-        population = self.contents
-        rmvlst = list()
+        population = self.contents # no copy
         lng = len(population)
 
         if nb > lng: return population
                 
-        rmvlst = random.choices(population, k=nb)
-        for it in rmvlst:
-            self.contents.remove(it) # remove from original array
+        rmvlst = random.sample(population, k=nb) # unique
+        for im in rmvlst:
+            population.remove(im) # removed from original array
 
         return rmvlst
         
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+def experiment(hat, 
+               expected_balls, 
+               num_balls_drawn, 
+               num_experiments):
+
+    population = hat.contents[:] # list
+    dtpop = hat.dtcontents.copy()
+    psize = len(population)
+
+    counts = expected_balls.copy() # dict
+    btrlst = [ k for k, v in counts.items() for _ in range(v) ]
+
+    nbd = num_balls_drawn
     probability = 1
+    
+    allblinbasketcheck = all( map( lambda k: counts[k] <= dtpop[k], counts ) )
+    if nbd > psize and allblinbasketcheck: 
+        return probability
+    
+    success = num_experiments    
+    tries = range(num_experiments)
+    for _ in tries:
+        smp = random.sample(population, k=nbd) # list
+        for bl in btrlst:
+            try:
+                smp.remove(bl)
+            except:
+                # print(bl, "not in smp")
+                success -= 1
+                break
+        # print(success)
+
+    # print("End of loop")
+
+    probability = success / num_experiments
     return probability
